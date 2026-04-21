ec8fdee9749af31150c8636ebbd7d289c110b042
Josh Cao <joshcaopdx@gmail.com>
2026-04-21 08:38:32 -0700

    Completed 9 possible epochs of training.

--- digits/in_depth_testing.py
+++ digits/in_depth_testing.py
@@ -18,7 +18,7 @@ def main():
         cost_function='cross-entropy'
     )
 
-    network.load("network_state_epoch_3.json")
+    network.load("network_state_epoch_2.json")
 
     k = 0
     correct = 0
