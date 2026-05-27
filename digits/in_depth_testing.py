926d9c692429d4a513d695dc7b8647185a8d4313
Josh Cao <joshcaopdx@gmail.com>
2026-05-27 16:02:42 -0700

    Epoch 5 stats in description
    Accuracy: 94.71%
    Loss: 1.6463

--- digits/in_depth_testing.py
+++ digits/in_depth_testing.py
@@ -19,7 +19,7 @@ def main():
         cost_function='cross-entropy'
     )
 
-    network.load("network_state_epoch_4.json")
+    network.load("network_state_epoch_5.json")
 
     k = 0
     batch_correct = 0
