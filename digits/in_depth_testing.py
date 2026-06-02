1e8d58f4dd6eb0f785926262f01fed222fbd8bed
Josh Cao <joshcaopdx@gmail.com>
2026-06-01 22:47:08 -0700

    Epoch 3 finished training
    Accuracy: 92.57%
    Loss: 1.6698

--- digits/in_depth_testing.py
+++ digits/in_depth_testing.py
@@ -19,7 +19,7 @@ def main():
         cost_function='cross-entropy'
     )
 
-    network.load("network_state_epoch_2.json")
+    network.load("network_state_epoch_3.json")
 
     k = 0
     batch_correct = 0
